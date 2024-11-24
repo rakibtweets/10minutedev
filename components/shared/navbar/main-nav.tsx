0a438b435c9159c85656c31054f20e079f9f966e
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-11-24 22:57:02 +0600

    feat(navbar): enhance navbar layout for improved responsiveness and styling

--- components/shared/navbar/main-nav.tsx
+++ components/shared/navbar/main-nav.tsx
@@ -7,14 +7,11 @@ import { cn } from '@/lib/utils';
 
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle
 } from '@/components/ui/navigation-menu';
-import { BookIcon } from 'lucide-react';
 
 const MainNav = () => {
   return (
