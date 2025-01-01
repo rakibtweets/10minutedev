285ba4c7a01446defde76d9052f3f878054a5907
Rakib Hasan <rakibhasanroky0@gmail.com>
2025-01-01 10:06:07 +0600

    feat(courses): add loading skeletons to CoursesWrapper for improved user experience

--- components/sections/CoursesWrapper.tsx
+++ components/sections/CoursesWrapper.tsx
@@ -2,12 +2,24 @@
 
 import { useSearchParams } from 'next/navigation';
 import Courses from './Courses';
+import { Suspense } from 'react';
+import CourseCardSkeleton from '../Skeletons/course-card-skelton';
 
 const CoursesWrapper = () => {
   const searchParams = useSearchParams();
   const tag = searchParams.get('tag') || '';
 
-  return <Courses tag={tag} />;
+  return (
+    <Suspense
+      fallback={Array(8)
+        .fill(0)
+        .map((_, index) => (
+          <CourseCardSkeleton key={index} />
+        ))}
+    >
+      <Courses tag={tag} />
+    </Suspense>
+  );
 };
 
 export default CoursesWrapper;
