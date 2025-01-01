3f1397ebd290a493c4042c328d4048c1f73d5661
Rakib Hasan <rakibhasanroky0@gmail.com>
2025-01-01 10:22:47 +0600

    fix(courses): add fallback to Suspense in CoursesLayout for improved loading behavior

--- app/(root)/courses/layout.tsx
+++ app/(root)/courses/layout.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 
 const CoursesLayout = ({ children }: { children: React.ReactNode }) => {
-  return <Suspense>{children}</Suspense>;
+  return <Suspense fallback={null}>{children}</Suspense>;
 };
 export default CoursesLayout;
